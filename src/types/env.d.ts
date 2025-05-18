dc207181ee8d644710327e8b66c04645388ef46c
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-05-18 08:23:45 +0200

    wip

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,8 @@
 interface ImportMetaEnv {
+    readonly MODE: string;
     readonly VITE_API_URL: string;
+    readonly VITE_DEV_BASE_URL: string;
+    readonly VITE_PROD_BASE_URL: string;
     readonly VITE_OTHER_ENV_VAR: string;
 }
 
